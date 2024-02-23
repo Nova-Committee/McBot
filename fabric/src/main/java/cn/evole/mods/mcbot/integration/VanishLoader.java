64362bc83fa9a296ca6143f2035f9d10f4aefeb4
Nacho <kulujun@gmail.com>
2024-02-23 01:40:30 +0000

    fix

--- fabric/src/main/java/cn/evole/mods/mcbot/integration/VanishLoader.java
+++ fabric/src/main/java/cn/evole/mods/mcbot/integration/VanishLoader.java
@@ -1,21 +1,26 @@
 package cn.evole.mods.mcbot.integration;
 
-import cn.evole.mods.mcbot.init.compat.VanishCompat;
-import me.drex.vanish.util.VanishManager;
-import com.mojang.authlib.GameProfile;
-import net.minecraft.server.level.ServerPlayer;
-import java.util.UUID;
+
+//兼容1.20.1版本vanish
+//#if MC == 12001
+//$$ import cn.evole.mods.mcbot.init.compat.VanishCompat;
+//$$ import me.drex.vanish.util.VanishManager;
+//$$ import com.mojang.authlib.GameProfile;
+//$$ import net.minecraft.server.level.ServerPlayer;
+//$$ import java.util.UUID;
+//#endif
 
 public class VanishLoader {
     public static boolean ISLOADED;
-
-    public static boolean isVanished(ServerPlayer player) {
-        if(!ISLOADED) return false;
-        // 获取玩家的 GameProfile
-        GameProfile gameProfile = player.getGameProfile();
-        // 从 GameProfile 中获取 UUID
-        UUID uuid = gameProfile.getId();
-        // 使用 VanishManager.isVanished 方法检查玩家是否处于隐身状态
-        return VanishCompat.VANISH && VanishManager.isVanished(player.getServer(), uuid);
-    }
+    //#if MC == 12001
+    //$$ public static boolean isVanished(ServerPlayer player) {
+    //$$     if(!ISLOADED) return false;
+    //$$     // 获取玩家的 GameProfile
+    //$$     GameProfile gameProfile = player.getGameProfile();
+    //$$     // 从 GameProfile 中获取 UUID
+    //$$     UUID uuid = gameProfile.getId();
+    //$$     // 使用 VanishManager.isVanished 方法检查玩家是否处于隐身状态
+    //$$     return VanishCompat.VANISH && VanishManager.isVanished(player.getServer(), uuid);
+    //$$ }
+    //#endif
 }
