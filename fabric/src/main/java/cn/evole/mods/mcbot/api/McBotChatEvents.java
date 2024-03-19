dbf66bcbd5dd9e6357d4069f7dba99bbfb771af4
xia-mc <2052472631@qq.com>
2024-03-19 14:37:02 +0800

    api修正

--- fabric/src/main/java/cn/evole/mods/mcbot/api/McBotChatEvents.java
+++ fabric/src/main/java/cn/evole/mods/mcbot/api/McBotChatEvents.java
@@ -9,14 +9,14 @@ public class McBotChatEvents {
      * 当玩家发送一条消息（并转发到QQ）后触发。
      * 包含发送消息的玩家和message_id
      */
-    public static final Event<PlayerChat> ON_CHAT = EventFactory.createArrayBacked(PlayerChat.class, callbacks -> (player, message_id) -> {
+    public static final Event<PlayerChat> ON_CHAT = EventFactory.createArrayBacked(PlayerChat.class, callbacks -> (player, message_id, message) -> {
         for (PlayerChat callback : callbacks) {
-            callback.onChat(player, message_id);
+            callback.onChat(player, message_id, message);
         }
     });
 
     @FunctionalInterface
     public interface PlayerChat {
-        void onChat(ServerPlayer player, int message_id);
+        void onChat(ServerPlayer player, int message_id, String message);
     }
 }
